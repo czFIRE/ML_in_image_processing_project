463d94611b615d69c088ac8161f0b9e3a4e57214
Petr Kadlec <xkadlec6@fi.muni.cz>
2022-04-15 15:53:55 +0200

    Fix: Properly implemented dataset downloading

--- dataset/downloader.py
+++ dataset/downloader.py
@@ -1,17 +1,19 @@
+import wget
 import os
+
 import shutil
 
 basic_url: str = "https://data.vision.ee.ethz.ch/cvl/DIV2K/"
 index_file_path: str = "datasets.txt"
 
-file = open(index_file_path, "r")
+print("\nDownloading and extracting dataset to this directory.\n")
 
-print("Downloading and extracting dataset to this directory.")
+with open(index_file_path, "r") as file:
+    for line in file.readlines():
+        # if you don't have wget module then uncomment the next line and comment the wget command
+        # os.system("wget " + basic_url + line[:-1] + ".zip")
+        wget.download(basic_url + line[:-1] + ".zip")
 
-for line in file.readlines():
-    os.system("wget " + basic_url + line[:-1] + ".zip")
-    shutil.unpack_archive("./" + line[:-1] + ".zip")
+        shutil.unpack_archive("./" + line[:-1] + ".zip")
 
-file.close()
-
-print("All done!")
+print("\nAll done!")
